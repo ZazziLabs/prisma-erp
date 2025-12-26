10ee6377152d51c7a6430f4be4ec7086f1b3404e
Davi Aguiar <zazzistore.loja@gmail.com>
2025-12-26 00:08:14 +0000

    build

---
+++ dist/assets/Input-BRFvc_qE.js
@@ -0,0 +1,11 @@
+import{c as s,j as t}from"./index-DkXwHJzI.js";/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],l=s("plus",o);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M10 11v6",key:"nco0om"}],["path",{d:"M14 11v6",key:"outv1u"}],["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],i=s("trash-2",n),u=({label:e,error:a,className:r="",...d})=>t.jsxs("div",{className:"w-full",children:[e&&t.jsx("label",{className:"block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1",children:e}),t.jsx("input",{className:`w-full p-3 bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-xl focus:ring-2 focus:ring-purple-500 focus:outline-none transition-all ${a?"border-red-500":""} ${r} dark:text-white`,...d}),a&&t.jsx("p",{className:"text-xs text-red-500 mt-1",children:a})]});export{u as I,l as P,i as T};
